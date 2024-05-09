bcaa84b8e3319921ffb44c5af77b04e2156f7d88
Daniel Ostertag <daniel.ostertag@avispl.com>
2024-05-09 15:30:09 -0500

    moved trailing spaces error to warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,7 +49,7 @@ module.exports = {
       'never'
     ],
     'eqeqeq': 'error',
-    'no-trailing-spaces': 'error',
+    'no-trailing-spaces': 'warn',
     'object-curly-spacing': [
       'error', 'always'
     ],
