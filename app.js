80274b835c5f8597e8e5b32f6acaf088bf669ffa
Daniel Ostertag <daniel.ostertag@avispl.com>
2024-05-09 15:46:48 -0500

    corrected static keyword for express app

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-import express, { static } from "express";
-const app = express();
+import express from 'express'
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5001;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 5001
 
-app.use(static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  console.log(`server started on port ${PORT}`)
+})
